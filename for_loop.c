9936b14975e5c96661a10ca79ab563276b1ca5c6
dostix <dominik.stoelting@stud.thga.de>
2023-05-29 11:43:55 +0200

    for-schleife hinzugefügt

--- for_loop.c
+++ for_loop.c
@@ -4,4 +4,11 @@ int main ()
 {
     char kette[30];
     strcpy(kette, "Hello World");
+
+    int zahl;
+    for (zahl=0;zahl<30;zahl++)
+    {
+        printf(" %s \n", kette[zahl]);
+    }
+    return 0;
 }
